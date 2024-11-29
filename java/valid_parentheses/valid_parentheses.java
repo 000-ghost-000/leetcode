bbf7a8816e267327bfbd6453dd2f0a07fcdecd9e
abhishek-0- <abhishek.srivastava2@s.amity.edu>
2024-11-30 00:47:17 +0530

    updated valid parentheses

--- java/valid_parentheses/valid_parentheses.java
+++ java/valid_parentheses/valid_parentheses.java
@@ -1,25 +1,36 @@
 import java.util.*;
+
 class Solution {
     public boolean isValid(String s) {
-        HashMap <Character , Character> para=new HashMap<>();
-        para.put(')','(');
-        para.put(']','[');
-        para.put('}','{');
-        char stack[]=new char[s.length()/2];
-        int x=-1;
-        for (int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{')
-            {
-                x++;
-                stack[x]=s.charAt(i);
+        // HashMap to map closing brackets to their corresponding opening brackets
+        HashMap<Character, Character> para = new HashMap<>();
+        para.put(')', '(');
+        para.put(']', '[');
+        para.put('}', '{');
+        
+        // Stack to hold opening brackets
+        Stack<Character> stack = new Stack<>();
+        
+        // Iterate through the string
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            
+            // If the current character is an opening bracket, push it onto the stack
+            if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
+                stack.push(currentChar);
             }
-            else if (stack[x]==para.get(s.charAt(i)))
-            {
-                stack[x]='\0';
-                x--;
+            // If it's a closing bracket, check if it matches the last opening bracket
+            else if (para.containsKey(currentChar)) {
+                // Check if the stack is empty or the top of the stack does not match
+                if (stack.isEmpty() || stack.peek() != para.get(currentChar)) {
+                    return false;
+                }
+                // Pop the matching opening bracket
+                stack.pop();
             }
         }
-       return (x==-1);
+        
+        // If the stack is empty, it means all brackets are properly matched
+        return stack.isEmpty();
     }
 }
